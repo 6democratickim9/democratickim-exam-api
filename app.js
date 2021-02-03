21103af42a5dbaafe9880bd4608312e9ce35e57e
6democratickim9 <shcomon24@gmail.com>
2021-02-03 17:12:41 +0900

    endline

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000
+// http://127.0.0.1:3000/
+app.get('/',(req,res) => res.send('WELCOME!'));
+app.get('/sevice',(req,res) =>  {
+    var user_name = req.param('Status')
+    res.send('The system is running [Status: ON]');
+});
+app.get('/check',(req,res) =>  res.send("NOMNOMNOMONOMONOMNOMONONO"));
+app.listen(port,() => console.log('Example app listening on port 3000'))
